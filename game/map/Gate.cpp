9ddc02b2a0d1d41f849d584c99a0cfbecee86f24
roganOhDev <fdscbjdcnhd@gmail.com>
2023-05-25 11:38:57 +0900

    create gate

---
+++ game/map/Gate.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Oh DongGeun on 2023/05/25.
+//
+
+#include "Gate.h"
---
+++ game/map/Gate.h
@@ -0,0 +1,16 @@
+//
+// Created by Oh DongGeun on 2023/05/25.
+//
+
+#ifndef SNAKEGAME_GATE_H
+#define SNAKEGAME_GATE_H
+
+
+
+class Gate {
+
+};
+
+
+
+#endif //SNAKEGAME_GATE_H
